7ab44da0ddad226011ca5187327734db3b14a116
Ngo Quoc Anh <anhngoquoc@gmail.com>
2016-01-12 21:55:29 +0700

    update properties file and mapper

--- pikojx/src/main/java/com/akoscode/pikojx/dao/ContactDAO.java
+++ pikojx/src/main/java/com/akoscode/pikojx/dao/ContactDAO.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +13,30 @@ import com.akoscode.pikojx.model.Contact;
 @Component
 public class ContactDAO extends BaseDao {
 	
+	@Value("${sql.contact.select.all}")
+	private String sqlSelectAll;
+	
+	protected static class ContactMapper implements RowMapper<Contact> {
+
+		public void mapRow(Contact contact, ResultSet rs, int rowNum) throws SQLException {
+			contact.setId(rs.getInt("contact_id"));
+			contact.setName(rs.getString("name"));
+			contact.setEmail(rs.getString("email"));
+			contact.setAddress(rs.getString("address"));
+			contact.setTelephone(rs.getString("telephone"));
+		}
+		
+		@Override
+		public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Contact contact = new Contact();
+			mapRow(contact, rs, rowNum);
+			return contact;
+		}
+		
+	}
+	
 	public List<Contact> selectAll() {
-    	String sql = "SELECT * FROM contact";
-    	List<Contact> listContact = super.getJdbcTemplate().query(sql, new RowMapper<Contact>() {
-    		 
-            @Override
-            public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Contact aContact = new Contact();
-     
-                aContact.setId(rs.getInt("contact_id"));
-                aContact.setName(rs.getString("name"));
-                aContact.setEmail(rs.getString("email"));
-                aContact.setAddress(rs.getString("address"));
-                aContact.setTelephone(rs.getString("telephone"));
-     
-                return aContact;
-            }
-     
-        });
-     
+    	List<Contact> listContact = super.getJdbcTemplate().query(sqlSelectAll, new ContactMapper());
         return listContact;
     }
     
